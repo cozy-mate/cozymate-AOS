54e3bd841dcd45d57477b988653554c8aca13797
youngseojang <youngseojang@gmail.com>
2024-07-17 23:27:39 +0900

    feat: 디렉토리 구조 변경 및 프래그먼트 코드 추가

--- app/src/main/java/umc/cozymate/ui/MainActivity.kt
+++ app/src/main/java/umc/cozymate/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package umc.cozymate
+package umc.cozymate.ui
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import umc.cozymate.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/umc/cozymate/ui/my_page/MyPageFragment.kt
@@ -0,0 +1,4 @@
+package umc.cozymate.ui.my_page
+
+class MyPageFragment {
+}
