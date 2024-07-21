5b00a9da0c9c62d8b708337a1f029c68cccda6f2
iwannacat <ju0808lie@naver.com>
2024-07-22 05:09:45 +0900

    [COZY-75]feat: todo 체크 박스 추가

--- app/src/main/java/umc/cozymate/ui/role_rule/TodoList.kt
+++ app/src/main/java/umc/cozymate/ui/role_rule/TodoList.kt
@@ -1,5 +1,7 @@
 package umc.cozymate.ui.role_rule
 
 data class TodoList(
-    var title: String
+    var id: Int = 0,
+    var title: String,
+    var isChecked: Boolean
 )
--- app/src/main/java/umc/cozymate/ui/role_rule/TodoTabFragment.kt
+++ app/src/main/java/umc/cozymate/ui/role_rule/TodoTabFragment.kt
@@ -5,19 +5,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
 import umc.cozymate.databinding.FragmentTodoTabBinding
 
 class TodoTabFragment: Fragment() {
 //    private var _binding: FragmentTodoTabBinding? = null
 //    private val binding get() = _binding!!
     lateinit var binding: FragmentTodoTabBinding
+    private var todoList= ArrayList<TodoList>()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentTodoTabBinding.inflate(inflater, container, false)
-//        return super.onCreateView(inflater, container, savedInstanceState)
+        initListData()
+        initRecyclerview()
         return binding.root
     }
+
+    private fun initListData() {
+        todoList.apply{
+            add(TodoList(1,"test1",false))
+            add(TodoList(2,"test2",false))
+            add(TodoList(3,"test3",false))
+            add(TodoList(4,"test4",false))
+            add(TodoList(5,"test5",false))
+            add(TodoList(6,"test6",false))
+        }
+    }
+
+    private fun initRecyclerview(){
+        binding.rvMyTodoList.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        val todoRVAdapter = TodoListRVAdapter(todoList)
+        binding.rvMyTodoList.adapter = todoRVAdapter
+    }
 }
+
