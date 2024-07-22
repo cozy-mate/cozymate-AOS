ce16444d329b54cda3655a15efef559b268b080f
Dodi029 <4717jun57@naver.com>
2024-07-23 03:40:43 +0900

    [COZY-101]/feat: activity들 초기설정

--- app/src/main/java/umc/cozymate/ui/roommate/RoommateBasicInfoActivity.kt
+++ app/src/main/java/umc/cozymate/ui/roommate/RoommateBasicInfoActivity.kt
@@ -1,21 +1,12 @@
 package umc.cozymate.ui.roommate
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import umc.cozymate.R
 
 class RoommateBasicInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_roommate_basic_info)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
