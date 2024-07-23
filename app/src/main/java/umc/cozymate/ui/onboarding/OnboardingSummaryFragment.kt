fb5a93195f024aac0d1f94157f6b6480dd2ad13f
youngseojang <youngseojang@gmail.com>
2024-07-24 02:46:15 +0900

    feat: [온보딩]->[코지홈] 화면전환 적용

--- app/src/main/java/umc/cozymate/ui/onboarding/OnboardingSummaryFragment.kt
+++ app/src/main/java/umc/cozymate/ui/onboarding/OnboardingSummaryFragment.kt
@@ -1,11 +1,13 @@
 package umc.cozymate.ui.onboarding
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import umc.cozymate.databinding.FragmentOnboardingSummaryBinding
+import umc.cozymate.ui.MainActivity
 
 class OnboardingSummaryFragment : Fragment() {
     private val TAG = this.javaClass.simpleName
@@ -20,6 +22,10 @@ class OnboardingSummaryFragment : Fragment() {
     ): View {
         _binding = FragmentOnboardingSummaryBinding.inflate(inflater, container, false)
 
+        binding.btnNext.setOnClickListener {
+            var intent = Intent(activity, MainActivity::class.java)
+            startActivity(intent)
+        }
         return binding.root
     }
 }
