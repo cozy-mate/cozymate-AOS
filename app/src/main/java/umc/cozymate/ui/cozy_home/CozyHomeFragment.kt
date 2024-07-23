03b5fe0fe5e8264f0a3702479b003c9fa100084b
youngseojang <youngseojang@gmail.com>
2024-07-24 02:46:42 +0900

    feat: 버튼 글씨 크기 적용

--- app/src/main/java/umc/cozymate/ui/cozy_home/CozyHomeFragment.kt
+++ app/src/main/java/umc/cozymate/ui/cozy_home/CozyHomeFragment.kt
@@ -1,10 +1,16 @@
 package umc.cozymate.ui.cozy_home
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.text.style.TextAppearanceSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import umc.cozymate.R
 import umc.cozymate.databinding.FragmentCozyhomeBinding
 
 class CozyHomeFragment : Fragment() {
@@ -18,6 +24,28 @@ class CozyHomeFragment : Fragment() {
     ): View {
         _binding = FragmentCozyhomeBinding.inflate(inflater, container, false)
 
+        initView()
         return binding.root
+    }
+
+    private fun initView() {
+        val btnText = binding.btnRoommateInvite
+        val spannableString = SpannableString(btnText.text)
+
+        // 버튼 내 텍스트 스타일 변경
+        spannableString.setSpan(
+            TextAppearanceSpan(requireContext(), R.style.TextAppearance_App_12sp_Medium),
+            10,
+            btnText.text.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        val color = ContextCompat.getColor(requireContext(), R.color.main_blue)
+        spannableString.setSpan(
+            ForegroundColorSpan(color),
+            10,
+            btnText.text.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        btnText.text = spannableString
     }
 }
