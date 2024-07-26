a7b93e65749b5401640a8792db68ee79537c34bb
4717jun57 <4717jun57@naver.com>
2024-07-26 17:24:51 +0900

    [COZY-101]/feat: 박스 선택 시 색상 변경 및 유지 기능 구현 중

--- app/src/main/java/umc/cozymate/ui/roommate/RoommateBasicInfoActivity.kt
+++ app/src/main/java/umc/cozymate/ui/roommate/RoommateBasicInfoActivity.kt
@@ -1,12 +1,32 @@
 package umc.cozymate.ui.roommate
 
 import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import umc.cozymate.R
 
 class RoommateBasicInfoActivity : AppCompatActivity() {
+
+    private var selectedNumPeopleOption: TextView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_roommate_basic_info)
+
+        val numPeopleLayout: LinearLayout = findViewById(R.id.ly_dormitory)
+        val livingLayout: LinearLayout = findViewById(R.id.ly_dormitory)
+
+        for (i in 0 until numPeopleLayout.childCount) {
+            val option = numPeopleLayout.getChildAt(i) as TextView
+            option.setOnClickListener { onNumPeopleOptionSelected(it) }
+        }
     }
+    private fun onNumPeopleOptionSelected(view: View) {
+        selectedNumPeopleOption?.isSelected = false
+        selectedNumPeopleOption = view as TextView
+        selectedNumPeopleOption?.isSelected = true
+    }
+
 }
