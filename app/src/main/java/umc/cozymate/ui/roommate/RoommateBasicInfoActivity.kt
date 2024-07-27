890ad3d9fa083dd1f19335726822cbe3b3aca601
4717jun57 <4717jun57@naver.com>
2024-07-28 01:43:46 +0900

    [COZY-101]/feat: 기본 정보 입력 뷰 구현 완료
    선택한 값을 따로 저장하는데 오류 발생 중

--- app/src/main/java/umc/cozymate/ui/roommate/RoommateBasicInfoActivity.kt
+++ app/src/main/java/umc/cozymate/ui/roommate/RoommateBasicInfoActivity.kt
@@ -1,32 +1,85 @@
 package umc.cozymate.ui.roommate
 
 import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import umc.cozymate.R
+import umc.cozymate.databinding.ActivityMainBinding
+import umc.cozymate.databinding.ActivityRoommateBasicInfoBinding
+import umc.cozymate.databinding.FragmentRoommateBinding
 
 class RoommateBasicInfoActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityRoommateBasicInfoBinding
+
     private var selectedNumPeopleOption: TextView? = null
+    private var selectedLivingOption: TextView? = null
+
+    private var selectedNumPeople: String? = null
+    private var selectedLiving: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_roommate_basic_info)
 
-        val numPeopleLayout: LinearLayout = findViewById(R.id.ly_dormitory)
-        val livingLayout: LinearLayout = findViewById(R.id.ly_dormitory)
+        binding = ActivityRoommateBasicInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        for (i in 0 until numPeopleLayout.childCount) {
-            val option = numPeopleLayout.getChildAt(i) as TextView
-            option.setOnClickListener { onNumPeopleOptionSelected(it) }
+        val dormitoryLayout = binding.lyDormitory
+        val peopleNumLayout = binding.lyPeopleNumber
+        val progressBar = binding.pbBasic
+
+        progressBar.max = 100
+
+        for (i in 0 until dormitoryLayout.childCount) {
+            val option = dormitoryLayout.getChildAt(i) as TextView
+            option.setOnClickListener {
+                Log.d("Basic Info", "Num People Option Clicked: ${option.text}")
+                onNumPeopleOptionSelected(it)
+            }
+        }
+
+        for (i in 0 until peopleNumLayout.childCount) {
+            val option = peopleNumLayout.getChildAt(i) as TextView
+            option.setOnClickListener {
+                Log.d("Basic Info", "Living Option Clicked: ${option.text}")
+                onLivingOptionSelected(it)
+            }
         }
     }
+
     private fun onNumPeopleOptionSelected(view: View) {
         selectedNumPeopleOption?.isSelected = false
         selectedNumPeopleOption = view as TextView
         selectedNumPeopleOption?.isSelected = true
+
+        selectedNumPeople = when (selectedNumPeopleOption?.text.toString()) {
+            "2명" -> "TWO_PEOPLE"
+            "3명" -> "THREE_PEOPLE"
+            "4명" -> "FOUR_PEOPLE"
+            "5명" -> "Five"
+            "6명" -> "Six"
+            else -> null
+        }
+        Log.d("Basic Info", "Selected Num People: $selectedNumPeople")
     }
 
+    private fun onLivingOptionSelected(view: View) {
+        selectedLivingOption?.isSelected = false
+        selectedLivingOption = view as TextView
+        selectedLivingOption?.isSelected = true
+
+        selectedLiving = when (selectedLivingOption?.text.toString()) {
+            "합격" -> "COMMUTE"
+            "대기중" -> "LIVING_ALONE"
+            "예비번호를 받았어요!" -> "DORMITORY"
+            else -> null
+        }
+        Log.d("Basic Info", "Selected Living: $selectedLiving")
+    }
 }
