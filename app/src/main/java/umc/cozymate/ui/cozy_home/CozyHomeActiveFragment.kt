7568183aa91fc97257123f105b83e8cd334a6d52
youngseojang <youngseojang@gmail.com>
2024-07-30 02:31:19 +0900

    [COZY-44]feat: [코지홈_활성화] 디자인 반영 - 특정 글자만 색상 변경되도록 함

--- app/src/main/java/umc/cozymate/ui/cozy_home/CozyHomeActiveFragment.kt
+++ app/src/main/java/umc/cozymate/ui/cozy_home/CozyHomeActiveFragment.kt
@@ -1,10 +1,16 @@
 package umc.cozymate.ui.cozy_home
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.text.style.TextAppearanceSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import umc.cozymate.R
 import umc.cozymate.databinding.FragmentCozyhomeActiveBinding
 
 class CozyHomeActiveFragment : Fragment() {
@@ -21,5 +27,26 @@ class CozyHomeActiveFragment : Fragment() {
 
         // initView()
         return binding.root
+    }
+
+    private fun initView() {
+        val tvWhoseRoom = binding.tvWhoseRoom
+        val spannableString = SpannableString(tvWhoseRoom.text)
+
+        // 버튼 내 텍스트 스타일 변경
+        spannableString.setSpan(
+            TextAppearanceSpan(requireContext(), R.style.TextAppearance_App_18sp_SemiBold),
+            4,
+            tvWhoseRoom.text.length-7,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        val color = ContextCompat.getColor(requireContext(), R.color.main_blue)
+        spannableString.setSpan(
+            ForegroundColorSpan(color),
+            4,
+            tvWhoseRoom.text.length-7,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        tvWhoseRoom.text = spannableString
     }
 }
