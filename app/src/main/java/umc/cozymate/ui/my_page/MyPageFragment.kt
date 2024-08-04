6c137551e311e7193a30e9567720222d3e2c0d53
iwannacat <ju0808lie@naver.com>
2024-08-04 15:39:03 +0900

    [COZY-96] feat: test 버튼 추가

--- app/src/main/java/umc/cozymate/ui/my_page/MyPageFragment.kt
+++ app/src/main/java/umc/cozymate/ui/my_page/MyPageFragment.kt
@@ -1,11 +1,13 @@
 package umc.cozymate.ui.my_page
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import umc.cozymate.databinding.FragmentMypageBinding
+import umc.cozymate.ui.roommate.MessageActivity
 
 class MyPageFragment : Fragment() {
     private var _binding: FragmentMypageBinding? = null
@@ -18,6 +20,9 @@ class MyPageFragment : Fragment() {
     ): View {
         _binding = FragmentMypageBinding.inflate(inflater, container, false)
 
+        binding.btnTest.setOnClickListener {
+            startActivity(Intent(activity,MessageActivity::class.java))
+        }
         return binding.root
     }
 }
