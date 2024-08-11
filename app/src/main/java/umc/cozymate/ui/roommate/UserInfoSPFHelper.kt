4524df3557a04e1d5d7f72c5ad92b0124b6df889
Dodi029 <4717jun57@naver.com>
2024-08-11 20:26:47 +0900

    [COZY-154]/feat: 뷰SPF에 값 저장중

--- app/src/main/java/umc/cozymate/ui/roommate/UserInfoSPFHelper.kt
+++ app/src/main/java/umc/cozymate/ui/roommate/UserInfoSPFHelper.kt
@@ -5,9 +5,134 @@ import android.content.SharedPreferences
 import android.util.Log
 import umc.cozymate.ui.roommate.data_class.UserInfo
 
+//class UserInfoSPFHelper(context: Context) {
+//
+//    private val spf: SharedPreferences =
+//        context.getSharedPreferences("userInfo", Context.MODE_PRIVATE)
+//
+//    fun saveUserInfo(info: UserInfo) {
+//        with(spf.edit()) {
+//            putBoolean("login", info.login)
+//            putString("school", info.school)
+//
+//            putString("name", info.name)
+//            putString("birth", info.birth)
+//            putString("admissionYear", info.admissionYear)
+//            putString("major", info.major)
+//            putInt("numOfRoommate", info.numOfRoommate)
+//            putString("acceptance", info.acceptance)
+//
+//            putString("wakeAmPm", info.wakeAmPm)
+//            putInt("wakeUpTime", info.wakeUpTime)
+//            putString("sleepAmPm", info.sleepAmPm)
+//            putInt("sleepTime", info.sleepTime)
+//            putString("lightOffAmPm", info.lightOffAmPm)
+//            putInt("lightOffTime", info.lightOffTime)
+//
+//            putString("smokingState", info.smokingState)
+//            putString("sleepingHabit", info.sleepingHabit)
+//            putString("airConditioningIntensity", info.airConditioningIntensity)
+//            putString("heatingIntensity", info.heatingIntensity)
+//            putString("lifePattern", info.lifePattern)
+//            putString("intimacy", info.intimacy)
+//            putString("canShare", info.canShare)
+//            putString("isPlayGame", info.isPlayGame)
+//            putString("isPhoneCall", info.isPhoneCall)
+//            putString("sleepAmPm", info.studying)
+//            putString("intake", info.intake)
+//            putString("cleanSensitivity", info.cleanSensitivity)
+//            putString("noiseSensitivity", info.noiseSensitivity)
+//            putString("cleaningFrequency", info.cleaningFrequency)
+//            putString("personality", info.personality)
+//            putString("mbti", info.mbti)
+//
+//
+//
+//            commit()
+//            Log.d("SPFHelper", "User Info: $info")
+//        }
+//    }
+//
+//    fun loadUserInfo(): UserInfo {
+//        val login = spf.getBoolean("login", false) ?: false
+//        val school = spf.getString("school", "") ?: ""
+//        val name = spf.getString("name", "") ?: ""
+//        val birth = spf.getString("birth", "") ?: ""
+//        val admissionYear = spf.getString("admissionYear", "") ?: ""
+//        val major = spf.getString("major", "") ?: ""
+//        val numOnRoommate = spf.getInt("numOnRoommate", 0) ?: 0
+//        val acceptance = spf.getString("acceptance", "") ?: ""
+//
+//        val wakeAmPm = spf.getString("wakeAmPm", "") ?: ""
+//        val wakeUpTime = spf.getInt("wakeUpTime", 0) ?: 0
+//        val sleepAmPm = spf.getString("sleepAmPm", "") ?: ""
+//        val sleepTime = spf.getInt("sleepTime", 0)
+//        val lightOffAmPm = spf.getString("lightOffAmPm", "") ?: ""
+//        val lightOffTime = spf.getInt("lightOffTime", 0)
+//
+//        val smokingState = spf.getString("smokingState", "") ?: ""
+//        val sleepingHabit = spf.getString("sleepingHabit", "") ?: ""
+//
+//        val airConditioningIntensity = spf.getString("airConditioningIntensity", "") ?: ""
+//        val heatingIntensity = spf.getString("heatingIntensity", "") ?: ""
+//
+//        val lifePattern = spf.getString("lifePattern", "") ?: ""
+//        val intimacy = spf.getString("intimacy", "") ?: ""
+//        val canShare = spf.getString("canShare", "") ?: ""
+//        val isPlayGame = spf.getString("isPlayGame", "") ?: ""
+//        val isPhoneCall = spf.getString("isPhoneCall", "") ?: ""
+//        val studying = spf.getString("studying", "") ?: ""
+//        val intake = spf.getString("intake", "") ?: ""
+//        val cleanSensitivity = spf.getString("cleanSensitivity", "") ?: ""
+//        val noiseSensitivity = spf.getString("noiseSensitivity", "") ?: ""
+//        val cleaningFrequency = spf.getString("cleaningFrequency", "") ?: ""
+//        val personality = spf.getString("personality", "") ?: ""
+//        val mbti = spf.getString("mbti", "") ?: ""
+//
+//
+//        val info = UserInfo(
+//            login,
+//            school,
+//
+//            name,
+//            birth,
+//            admissionYear,
+//            major,
+//            numOnRoommate,
+//            acceptance,
+//
+//            wakeAmPm,
+//            wakeUpTime,
+//            sleepAmPm,
+//            sleepTime,
+//            lightOffAmPm,
+//            lightOffTime,
+//
+//            smokingState,
+//            sleepingHabit,
+//            airConditioningIntensity,
+//            heatingIntensity,
+//            lifePattern,
+//            intimacy,
+//            canShare,
+//            isPlayGame,
+//            isPhoneCall,
+//            studying,
+//            intake,
+//            cleanSensitivity,
+//            noiseSensitivity,
+//            cleaningFrequency,
+//            personality,
+//            mbti
+//        )
+//        Log.d("SPFHelper Load", "Loaded UserInfo: $info")
+//        return info
+//    }
+//}
 class UserInfoSPFHelper(context: Context) {
 
-    private val spf: SharedPreferences = context.getSharedPreferences("userInfo", Context.MODE_PRIVATE)
+    private val spf: SharedPreferences =
+        context.getSharedPreferences("userInfo", Context.MODE_PRIVATE)
 
     fun saveUserInfo(info: UserInfo) {
         with(spf.edit()) {
@@ -24,27 +149,65 @@ class UserInfoSPFHelper(context: Context) {
             putString("wakeAmPm", info.wakeAmPm)
             putInt("wakeUpTime", info.wakeUpTime)
             putString("sleepAmPm", info.sleepAmPm)
+            putInt("sleepTime", info.sleepTime)
+            putString("lightOffAmPm", info.lightOffAmPm)
+            putInt("lightOffTime", info.lightOffTime)
 
+            putString("smokingState", info.smokingState)
+            putString("sleepingHabit", info.sleepingHabit)
+            putString("airConditioningIntensity", info.airConditioningIntensity)
+            putString("heatingIntensity", info.heatingIntensity)
+            putString("lifePattern", info.lifePattern)
+            putString("intimacy", info.intimacy)
+            putString("canShare", info.canShare)
+            putString("isPlayGame", info.isPlayGame)
+            putString("isPhoneCall", info.isPhoneCall)
+            putString("studying", info.studying)
+            putString("intake", info.intake)
+            putString("cleanSensitivity", info.cleanSensitivity)
+            putString("noiseSensitivity", info.noiseSensitivity)
+            putString("cleaningFrequency", info.cleaningFrequency)
+            putString("personality", info.personality)
+            putString("mbti", info.mbti)
 
             commit()
             Log.d("SPFHelper", "User Info: $info")
         }
     }
 
-    fun loadUserInfo() : UserInfo{
-        val login = spf.getBoolean("login",false) ?: false
+    fun loadUserInfo(): UserInfo {
+        val login = spf.getBoolean("login", false) ?: false
         val school = spf.getString("school", "") ?: ""
         val name = spf.getString("name", "") ?: ""
         val birth = spf.getString("birth", "") ?: ""
         val admissionYear = spf.getString("admissionYear", "") ?: ""
         val major = spf.getString("major", "") ?: ""
-        val numOnRoommate = spf.getInt("numOnRoommate", 0) ?: 0
+        val numOfRoommate = spf.getInt("numOfRoommate", 0) ?: 0
         val acceptance = spf.getString("acceptance", "") ?: ""
 
         val wakeAmPm = spf.getString("wakeAmPm", "") ?: ""
         val wakeUpTime = spf.getInt("wakeUpTime", 0) ?: 0
         val sleepAmPm = spf.getString("sleepAmPm", "") ?: ""
+        val sleepTime = spf.getInt("sleepTime", 0)
+        val lightOffAmPm = spf.getString("lightOffAmPm", "") ?: ""
+        val lightOffTime = spf.getInt("lightOffTime", 0)
 
+        val smokingState = spf.getString("smokingState", "") ?: ""
+        val sleepingHabit = spf.getString("sleepingHabit", "") ?: ""
+        val airConditioningIntensity = spf.getString("airConditioningIntensity", "") ?: ""
+        val heatingIntensity = spf.getString("heatingIntensity", "") ?: ""
+        val lifePattern = spf.getString("lifePattern", "") ?: ""
+        val intimacy = spf.getString("intimacy", "") ?: ""
+        val canShare = spf.getString("canShare", "") ?: ""
+        val isPlayGame = spf.getString("isPlayGame", "") ?: ""
+        val isPhoneCall = spf.getString("isPhoneCall", "") ?: ""
+        val studying = spf.getString("studying", "") ?: ""
+        val intake = spf.getString("intake", "") ?: ""
+        val cleanSensitivity = spf.getString("cleanSensitivity", "") ?: ""
+        val noiseSensitivity = spf.getString("noiseSensitivity", "") ?: ""
+        val cleaningFrequency = spf.getString("cleaningFrequency", "") ?: ""
+        val personality = spf.getString("personality", "") ?: ""
+        val mbti = spf.getString("mbti", "") ?: ""
 
         val info = UserInfo(
             login,
@@ -53,12 +216,30 @@ class UserInfoSPFHelper(context: Context) {
             birth,
             admissionYear,
             major,
-            numOnRoommate,
+            numOfRoommate,
             acceptance,
-
             wakeAmPm,
             wakeUpTime,
-            sleepAmPm
+            sleepAmPm,
+            sleepTime,
+            lightOffAmPm,
+            lightOffTime,
+            smokingState,
+            sleepingHabit,
+            airConditioningIntensity,
+            heatingIntensity,
+            lifePattern,
+            intimacy,
+            canShare,
+            isPlayGame,
+            isPhoneCall,
+            studying,
+            intake,
+            cleanSensitivity,
+            noiseSensitivity,
+            cleaningFrequency,
+            personality,
+            mbti
         )
         Log.d("SPFHelper Load", "Loaded UserInfo: $info")
         return info
--- app/src/main/java/umc/cozymate/ui/roommate/lifestyle_info/BasicInfoFragment.kt
+++ app/src/main/java/umc/cozymate/ui/roommate/lifestyle_info/BasicInfoFragment.kt
@@ -19,6 +19,257 @@ import umc.cozymate.ui.roommate.RoommateInputInfoActivity
 import umc.cozymate.ui.roommate.UserInfoSPFHelper
 import umc.cozymate.ui.roommate.data_class.UserInfo
 
+//class BasicInfoFragment : Fragment() {
+//
+//    private lateinit var binding: FragmentBasicInfoBinding
+//    private lateinit var spfHelper: UserInfoSPFHelper
+//
+//    private var userInfo = UserInfo()
+//
+//    private var onLivingOption: TextView? = null
+//    private var onLiving: String? = null
+//    private var numPeopleOption: TextView? = null
+//    private var numPeople: Int? = 2
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater, container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        binding = FragmentBasicInfoBinding.inflate(inflater, container, false)
+//
+//        // Restore state if available
+//        savedInstanceState?.let {
+//            onLiving = it.getString("onLiving")
+//            numPeople = it.getInt("numPeople")
+//            binding.etNumber.setText(it.getString("number"))
+//            binding.etBirth.setText(it.getString("birth"))
+//            binding.etName.setText(it.getString("name"))
+//            binding.etMajor.setText(it.getString("major"))
+//        }
+//
+//        spfHelper = (activity as RoommateInputInfoActivity).getUserInfoSPFHelper()
+//
+//        initTextChangeListener()
+//
+//        initLivingSelector()
+//        initNumPeoPleSelector()
+//
+//        // Add touch listener to the scroll view to hide the keyboard when tapping outside
+//        binding.nestedScrollView.setOnTouchListener { _, _ ->
+//            removeEditTextFocus()
+//            false
+//        }
+//
+//        // Update the next button state initially
+//        updateNextButtonState()
+//
+//        return binding.root
+//    }
+//
+//    private fun initTextChangeListener() {
+//        // Initialize EditText fields with TextWatchers
+//        binding.etNumber.addTextChangedListener(createTextWatcher())
+//        binding.etBirth.addTextChangedListener(createTextWatcher())
+//        binding.etName.addTextChangedListener(createTextWatcher())
+//        binding.etMajor.addTextChangedListener(createMajorTextWatcher())
+//    }
+//
+//    private fun initLivingSelector() {
+//        // Initialize options with click listeners
+//        val onLivingTexts = listOf(
+//            binding.dormitoryPass to "합격",
+//            binding.dormitoryWaiting to "대기중",
+//            binding.dormitoryNumber to "예비번호"
+//        )
+//        for ((textView, value) in onLivingTexts) {
+//
+//            textView.setOnClickListener { onLivingOptionSelected(it, value) }
+//        }
+//    }
+//
+//    private fun initNumPeoPleSelector() {
+//        val numPeopleTexts = listOf(
+//            binding.num2 to 2,
+//            binding.num3 to 3,
+//            binding.num4 to 4,
+//            binding.num5 to 5,
+//            binding.num6 to 6
+//        )
+//        for ((textView, value) in numPeopleTexts) {
+//            textView.setOnClickListener {
+//                numPeopleSelected(it, value)
+//            }
+//        }
+//
+//    }
+//
+//    override fun onSaveInstanceState(outState: Bundle) {
+//        super.onSaveInstanceState(outState)
+//        outState.putString("onLiving", onLiving)
+//        outState.putInt("numPeople", numPeople!!)
+//        outState.putString("number", binding.etNumber.text.toString())
+//        outState.putString("birth", binding.etBirth.text.toString())
+//        outState.putString("name", binding.etName.text.toString())
+//        outState.putString("major", binding.etMajor.text.toString())
+//    }
+//
+//    private fun onLivingOptionSelected(view: View, value: String) {
+//        // Update UI for selected living option
+//        onLivingOption?.apply {
+//            setTextColor(resources.getColor(R.color.unuse_font, null))
+//            background =
+//                resources.getDrawable(R.drawable.custom_option_box_background_default, null)
+//        }
+//        onLivingOption = view as TextView
+//        onLivingOption?.apply {
+//            setTextColor(resources.getColor(R.color.main_blue, null))
+//            background =
+//                resources.getDrawable(R.drawable.custom_option_box_background_selected_6dp, null)
+//        }
+//        onLiving = value
+//        userInfo = userInfo.copy(acceptance = value)
+//        updateNextButtonState()
+//    }
+//
+//    private fun numPeopleSelected(view: View, value: Int) {
+//        // Update UI for selected people number
+//        numPeopleOption?.apply {
+//            setTextColor(resources.getColor(R.color.unuse_font, null))
+//            background =
+//                resources.getDrawable(R.drawable.custom_option_box_background_default, null)
+//        }
+//        numPeopleOption = view as TextView
+//        numPeopleOption?.apply {
+//            setTextColor(resources.getColor(R.color.main_blue, null))
+//            background =
+//                resources.getDrawable(R.drawable.custom_option_box_background_selected_6dp, null)
+//        }
+//        numPeople = value
+//        userInfo = userInfo.copy(numOfRoommate = value)
+//        showDormitoryLayout() // Show dormitory layout when a number is selected
+//        updateNextButtonState()
+//    }
+//
+//    fun updateNextButtonState() {
+//        // Check if all fields are filled to enable next button
+//        val isNextButtonEnabled = onLiving != null &&
+//                numPeople != null &&
+//                binding.etMajor.text?.toString()?.isNotEmpty() == true &&
+//                binding.etNumber.text?.toString()?.isNotEmpty() == true &&
+//                binding.etBirth.text?.toString()?.isNotEmpty() == true &&
+//                binding.etName.text?.toString()?.isNotEmpty() == true
+//
+//        if (isNextButtonEnabled) {
+//            (activity as? RoommateInputInfoActivity)?.showNextButton()
+//        }
+//    }
+//
+//    private fun createTextWatcher(): TextWatcher {
+//        return object : TextWatcher {
+//            override fun afterTextChanged(s: Editable?) {
+//                userInfo = userInfo.copy(
+//                    // EditText의 내용으로 `userInfo` 업데이트
+//                    birth = binding.etBirth.text.toString(),
+//                    name = binding.etName.text.toString(),
+//                    major = binding.etMajor.text.toString()
+//                )
+//                showMajorLayout()
+//            }
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+//        }
+//    }
+//
+//
+//    private fun createMajorTextWatcher(): TextWatcher {
+//        return object : TextWatcher {
+//            override fun afterTextChanged(s: Editable?) {
+//                showPeopleNumberLayout()
+//                updateNextButtonState()
+//            }
+//
+//            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+//            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+//        }
+//    }
+//
+//    private fun showMajorLayout() {
+//        // Show the major layout when all initial fields are filled
+//        if (binding.etNumber.text?.isNotEmpty() == true &&
+//            binding.etBirth.text?.isNotEmpty() == true &&
+//            binding.etName.text?.isNotEmpty() == true
+//        ) {
+//            binding.clMajor.showWithSlideDownAnimation()
+//            scrollToTop()
+//        }
+//    }
+//
+//    private fun showPeopleNumberLayout() {
+//        // Show the people number layout when major is filled
+//        if (binding.etMajor.text?.isNotEmpty() == true) {
+//            binding.clPeopleNumber.showWithSlideDownAnimation()
+//            scrollToTop()
+//        }
+//    }
+//
+//    private fun showDormitoryLayout() {
+//        // Show the dormitory layout when people number is selected
+//        binding.clDormitory.showWithSlideDownAnimation()
+//        scrollToTop()
+//    }
+//
+//    private fun scrollToTop() {
+//        // Scroll the NestedScrollView to the top
+//        binding.nestedScrollView.scrollTo(0, 0)
+//    }
+//
+//    private fun hideKeyboard() {
+//        // Hide the keyboard
+//        val inputMethodManager =
+//            requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+//        inputMethodManager.hideSoftInputFromWindow(binding.root.windowToken, 0)
+//    }
+//
+//    private fun removeEditTextFocus() {
+//        // Clear focus from all EditTexts
+//        binding.etNumber.clearFocus()
+//        binding.etBirth.clearFocus()
+//        binding.etName.clearFocus()
+//        binding.etMajor.clearFocus()
+//
+//        // Hide the keyboard
+//        hideKeyboard()
+//    }
+//
+//    fun View.showWithSlideDownAnimation(duration: Long = 900) {
+//        this.apply {
+//            // 애니메이션 설정
+//            val slideDown = TranslateAnimation(0f, 0f, -this.height.toFloat(), 0f).apply {
+//                this.duration = duration
+//            }
+//
+//            val fadeIn = AlphaAnimation(0f, 1f).apply {
+//                this.duration = duration
+//            }
+//
+//            val animationSet = AnimationSet(true).apply {
+//                addAnimation(slideDown)
+//                addAnimation(fadeIn)
+//            }
+//
+//            // visibility를 VISIBLE로 설정
+//            visibility = View.VISIBLE
+//
+//            // 애니메이션 시작
+//            startAnimation(animationSet)
+//        }
+//    }
+//    fun saveUserInfo() {
+//        spfHelper.saveUserInfo(userInfo)
+//    }
+//}
+
+
 class BasicInfoFragment : Fragment() {
 
     private lateinit var binding: FragmentBasicInfoBinding
@@ -37,6 +288,11 @@ class BasicInfoFragment : Fragment() {
     ): View {
         binding = FragmentBasicInfoBinding.inflate(inflater, container, false)
 
+        spfHelper = (activity as RoommateInputInfoActivity).getUserInfoSPFHelper()
+
+        // Load saved userInfo from SharedPreferences
+        userInfo = spfHelper.loadUserInfo()
+
         // Restore state if available
         savedInstanceState?.let {
             onLiving = it.getString("onLiving")
@@ -45,22 +301,25 @@ class BasicInfoFragment : Fragment() {
             binding.etBirth.setText(it.getString("birth"))
             binding.etName.setText(it.getString("name"))
             binding.etMajor.setText(it.getString("major"))
+        } ?: run {
+            // If no savedInstanceState, load from SharedPreferences
+            onLiving = userInfo.acceptance
+            numPeople = userInfo.numOfRoommate
+            binding.etNumber.setText(userInfo.admissionYear)
+            binding.etBirth.setText(userInfo.birth)
+            binding.etName.setText(userInfo.name)
+            binding.etMajor.setText(userInfo.major)
         }
 
-        spfHelper = (activity as RoommateInputInfoActivity).getUserInfoSPFHelper()
-
         initTextChangeListener()
-
         initLivingSelector()
         initNumPeoPleSelector()
 
-        // Add touch listener to the scroll view to hide the keyboard when tapping outside
         binding.nestedScrollView.setOnTouchListener { _, _ ->
             removeEditTextFocus()
             false
         }
 
-        // Update the next button state initially
         updateNextButtonState()
 
         return binding.root
@@ -82,7 +341,6 @@ class BasicInfoFragment : Fragment() {
             binding.dormitoryNumber to "예비번호"
         )
         for ((textView, value) in onLivingTexts) {
-
             textView.setOnClickListener { onLivingOptionSelected(it, value) }
         }
     }
@@ -100,7 +358,6 @@ class BasicInfoFragment : Fragment() {
                 numPeopleSelected(it, value)
             }
         }
-
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -117,17 +374,16 @@ class BasicInfoFragment : Fragment() {
         // Update UI for selected living option
         onLivingOption?.apply {
             setTextColor(resources.getColor(R.color.unuse_font, null))
-            background =
-                resources.getDrawable(R.drawable.custom_option_box_background_default, null)
+            background = resources.getDrawable(R.drawable.custom_option_box_background_default, null)
         }
         onLivingOption = view as TextView
         onLivingOption?.apply {
             setTextColor(resources.getColor(R.color.main_blue, null))
-            background =
-                resources.getDrawable(R.drawable.custom_option_box_background_selected_6dp, null)
+            background = resources.getDrawable(R.drawable.custom_option_box_background_selected_6dp, null)
         }
         onLiving = value
         userInfo = userInfo.copy(acceptance = value)
+        spfHelper.saveUserInfo(userInfo)  // Save updated value
         updateNextButtonState()
     }
 
@@ -135,17 +391,16 @@ class BasicInfoFragment : Fragment() {
         // Update UI for selected people number
         numPeopleOption?.apply {
             setTextColor(resources.getColor(R.color.unuse_font, null))
-            background =
-                resources.getDrawable(R.drawable.custom_option_box_background_default, null)
+            background = resources.getDrawable(R.drawable.custom_option_box_background_default, null)
         }
         numPeopleOption = view as TextView
         numPeopleOption?.apply {
             setTextColor(resources.getColor(R.color.main_blue, null))
-            background =
-                resources.getDrawable(R.drawable.custom_option_box_background_selected_6dp, null)
+            background = resources.getDrawable(R.drawable.custom_option_box_background_selected_6dp, null)
         }
         numPeople = value
         userInfo = userInfo.copy(numOfRoommate = value)
+        spfHelper.saveUserInfo(userInfo)  // Save updated value
         showDormitoryLayout() // Show dormitory layout when a number is selected
         updateNextButtonState()
     }
@@ -168,18 +423,18 @@ class BasicInfoFragment : Fragment() {
         return object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
                 userInfo = userInfo.copy(
-                    // EditText의 내용으로 `userInfo` 업데이트
                     birth = binding.etBirth.text.toString(),
                     name = binding.etName.text.toString(),
                     major = binding.etMajor.text.toString()
                 )
+                spfHelper.saveUserInfo(userInfo)  // Save updated value
                 showMajorLayout()
             }
+
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
         }
     }
-
 
     private fun createMajorTextWatcher(): TextWatcher {
         return object : TextWatcher {
@@ -194,7 +449,6 @@ class BasicInfoFragment : Fragment() {
     }
 
     private fun showMajorLayout() {
-        // Show the major layout when all initial fields are filled
         if (binding.etNumber.text?.isNotEmpty() == true &&
             binding.etBirth.text?.isNotEmpty() == true &&
             binding.etName.text?.isNotEmpty() == true
@@ -205,7 +459,6 @@ class BasicInfoFragment : Fragment() {
     }
 
     private fun showPeopleNumberLayout() {
-        // Show the people number layout when major is filled
         if (binding.etMajor.text?.isNotEmpty() == true) {
             binding.clPeopleNumber.showWithSlideDownAnimation()
             scrollToTop()
@@ -213,61 +466,50 @@ class BasicInfoFragment : Fragment() {
     }
 
     private fun showDormitoryLayout() {
-        // Show the dormitory layout when people number is selected
         binding.clDormitory.showWithSlideDownAnimation()
         scrollToTop()
     }
 
     private fun scrollToTop() {
-        // Scroll the NestedScrollView to the top
         binding.nestedScrollView.scrollTo(0, 0)
     }
 
     private fun hideKeyboard() {
-        // Hide the keyboard
         val inputMethodManager =
             requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inputMethodManager.hideSoftInputFromWindow(binding.root.windowToken, 0)
     }
 
     private fun removeEditTextFocus() {
-        // Clear focus from all EditTexts
         binding.etNumber.clearFocus()
         binding.etBirth.clearFocus()
         binding.etName.clearFocus()
         binding.etMajor.clearFocus()
-
-        // Hide the keyboard
         hideKeyboard()
     }
 
     fun View.showWithSlideDownAnimation(duration: Long = 900) {
         this.apply {
-            // 애니메이션 설정
             val slideDown = TranslateAnimation(0f, 0f, -this.height.toFloat(), 0f).apply {
                 this.duration = duration
             }
-
             val fadeIn = AlphaAnimation(0f, 1f).apply {
                 this.duration = duration
             }
-
             val animationSet = AnimationSet(true).apply {
                 addAnimation(slideDown)
                 addAnimation(fadeIn)
             }
-
-            // visibility를 VISIBLE로 설정
             visibility = View.VISIBLE
-
-            // 애니메이션 시작
             startAnimation(animationSet)
         }
     }
+
     fun saveUserInfo() {
         spfHelper.saveUserInfo(userInfo)
     }
 }
+
 
 //class BasicInfoFragment : Fragment() {
 //
