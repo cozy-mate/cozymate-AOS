5c7c94a47666f8ca4c364c19574cfa67d3ef0421
youngseojang <youngseojang@gmail.com>
2024-08-18 18:06:57 +0900

    [COZY-158]feat: 팝업 배경 적용 + 밝기 적용

--- app/src/main/java/umc/cozymate/ui/cozy_home/pop_up/ServerErrorPopUp.kt
+++ app/src/main/java/umc/cozymate/ui/cozy_home/pop_up/ServerErrorPopUp.kt
@@ -2,6 +2,8 @@ package umc.cozymate.ui.cozy_home.pop_up
 
 import android.app.AlertDialog
 import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
 import umc.cozymate.databinding.PopupInviteCodeFailBinding
@@ -22,7 +24,18 @@ class ServerErrorPopUp : DialogFragment() {
             dismiss()
         }
 
-        return builder.create()
+        val dialog = builder.create()
+
+        // 배경 투명 + 밝기 조절 (0.7)
+        dialog.window?.let { window ->
+            window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+            val layoutParams = window.attributes
+            layoutParams.alpha = 0.7f // 밝기를 설정 (0.0f ~ 1.0f)
+            window.attributes = layoutParams
+        }
+
+        return dialog
     }
 
     override fun onDestroyView() {
