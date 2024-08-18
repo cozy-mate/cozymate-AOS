5e359e35163762e2d49b3db08dce9c4de6e21a91
youngseojang <youngseojang@gmail.com>
2024-08-18 17:32:03 +0900

    [COZY-158]feat: 초대코드 뒤로가기 및 확인 버튼 동작 구현

--- app/src/main/java/umc/cozymate/ui/cozy_home/entering_room/CozyHomeEnteringInviteCodeFragment.kt
+++ app/src/main/java/umc/cozymate/ui/cozy_home/entering_room/CozyHomeEnteringInviteCodeFragment.kt
@@ -1,13 +1,15 @@
 package umc.cozymate.ui.cozy_home.entering_room
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import umc.cozymate.databinding.FragmentCozyHomeEnteringInviteCodeBinding
 
-// 플로우3 : "초대코드 입력창(1)" > 룸메이트 대기창(2) > 코지홈 입장창(3) > 코지홈 활성화창
+// 플로우3 : "초대코드 입력창(1)" > 성공/실패 팝업창 > 코지홈 활성화창
 class CozyHomeEnteringInviteCodeFragment : Fragment() {
 
     private var _binding: FragmentCozyHomeEnteringInviteCodeBinding? = null
@@ -26,9 +28,37 @@ class CozyHomeEnteringInviteCodeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         with(binding) {
+
+            // 확인 버튼 비활성화
+            btnNext.isEnabled = false
+
+            // 뒤로가기 버튼
+            ivBack.setOnClickListener {
+                requireActivity().finish()
+            }
+
+            // et에 초대코드 입력 시 확인 버튼 활성화
+            etRoomName.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+                override fun afterTextChanged(s: Editable?) {
+                    btnNext.isEnabled = !s.isNullOrEmpty()
+                }
+
+            })
+
+            // root 뷰 클릭시 포커스 해제
+            root.setOnClickListener {
+                etRoomName.clearFocus()
+            }
+
+            // 확인 버튼
             btnNext.setOnClickListener {
                 (activity as? CozyHomeEnteringInviteCodeActivity)?.loadFragment2()
             }
+
         }
     }
 }
