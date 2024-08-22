467e2bda2d187d81013208b476e3873ac2db1487
Dodi029 <4717jun57@naver.com>
2024-08-23 02:14:30 +0900

    룸메 온보딩 뷰 수정

--- app/src/main/java/umc/cozymate/ui/roommate/RoommateOnboardingFragment.kt
+++ app/src/main/java/umc/cozymate/ui/roommate/RoommateOnboardingFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
+import umc.cozymate.R
 import umc.cozymate.databinding.FragmentRoommateOnboardingBinding
 import umc.cozymate.ui.viewmodel.SplashViewModel
 
@@ -19,9 +20,9 @@ class RoommateOnboardingFragment : Fragment() {
     private val splashViewModel: SplashViewModel by viewModels()
     private val binding get() = _binding!!
 
-    private var myNickname : String = ""
+    private var myNickname: String = ""
 
-//    private var nickname = ""
+    //    private var nickname = ""
     private var persona = 0
 
     override fun onCreateView(
@@ -38,6 +39,21 @@ class RoommateOnboardingFragment : Fragment() {
             startActivity(intent)
         }
 
+        binding.btnGoCrew.setOnClickListener {
+//            (context as MainActivity).supportFragmentManager.beginTransaction()
+//                .replace(R.id.main_container, RoommateMakeCrewableFragment())
+//                .commitAllowingStateLoss()
+            val nextFragment = RoommateMakeCrewableFragment()  // 이동할 프래그먼트 생성
+
+// 프래그먼트 매니저를 사용하여 트랜잭션 시작
+            parentFragmentManager.beginTransaction()
+                .replace(
+                    R.id.main_container,
+                    nextFragment
+                )  // R.id.fragment_container는 현재 프래그먼트가 들어있는 레이아웃의 ID
+                .addToBackStack(null)  // 뒤로 가기 버튼을 눌렀을 때 이전 프래그먼트로 돌아갈 수 있도록 백스택에 추가
+                .commit()  // 트랜잭션 완료
+        }
         getPreference()
         binding.tvName1.text = myNickname
         binding.tvName2.text = myNickname
@@ -47,6 +63,6 @@ class RoommateOnboardingFragment : Fragment() {
 
     private fun getPreference() {
         val spf = requireContext().getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
-        myNickname =  spf.getString("user_nickname", "No user found").toString()
+        myNickname = spf.getString("user_nickname", "No user found").toString()
     }
 }
