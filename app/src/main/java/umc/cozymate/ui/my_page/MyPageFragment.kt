540d1d45cbf10b40c9235a62b26197f20d53aefc
youngseojang <youngseojang@gmail.com>
2024-08-29 03:07:52 +0900

    [COZY-127]feature: 로그아웃 api 뷰모델 구현

--- app/src/main/java/umc/cozymate/ui/my_page/MyPageFragment.kt
+++ app/src/main/java/umc/cozymate/ui/my_page/MyPageFragment.kt
@@ -1,15 +1,21 @@
 package umc.cozymate.ui.my_page
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import umc.cozymate.R
 import umc.cozymate.databinding.FragmentMypageBinding
+import umc.cozymate.ui.splash.SplashActivity
+import umc.cozymate.ui.viewmodel.MyPageViewModel
 
 class MyPageFragment : Fragment() {
+    private lateinit var viewModel: MyPageViewModel
     private var _binding: FragmentMypageBinding? = null
     private val binding get() = _binding!!
     private var persona : Int = 0
@@ -21,11 +27,45 @@ class MyPageFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentMypageBinding.inflate(inflater, container, false)
+        viewModel = ViewModelProvider(requireActivity()).get(MyPageViewModel::class.java)
         getPreference()
         binding.tvMypageUserName.text =nickname
         binding.ivMypageCharacter.setImageResource(initCharactor())
         binding.tvMypageRoomName.text = roomname
+        binding.tvSignOut.setOnClickListener {
+            performLogout()
+        }
         return binding.root
+    }
+
+    private fun performLogout() {
+        viewModel.logOut()
+
+        // 로딩 표시
+        binding.progressBar.visibility = View.VISIBLE
+
+        // 로그아웃 상태를 관찰
+        viewModel.isLogOutSuccess.observe(viewLifecycleOwner) { isLogoutSuccess ->
+            if (isLogoutSuccess) {
+                binding.progressBar.visibility = View.GONE  // 로딩중 UI 숨기기
+                goToSplashActivity()
+            }
+        }
+
+        // 에러 상태를 관찰
+        viewModel.errorResponse.observe(viewLifecycleOwner) { errorResponse ->
+            if (errorResponse != null) {
+                binding.progressBar.visibility = View.GONE  // 로딩중 UI 숨기기
+                // 에러 메시지 보여주기
+                Toast.makeText(requireContext(), "로그아웃 실패", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun goToSplashActivity() {
+        val intent = Intent(requireContext(), SplashActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        startActivity(intent)
     }
 
     private fun getPreference() {
