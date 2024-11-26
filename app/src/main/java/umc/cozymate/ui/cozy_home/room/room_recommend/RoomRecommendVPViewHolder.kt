e58305fd6963ade3dbbd6e6da660a50dff99f765
youngseojang <youngseojang@gmail.com>
2024-11-26 15:40:38 +0900

    feat: 방 추천 수정

--- app/src/main/java/umc/cozymate/ui/cozy_home/room/room_recommend/RoomRecommendVPViewHolder.kt
+++ app/src/main/java/umc/cozymate/ui/cozy_home/room/room_recommend/RoomRecommendVPViewHolder.kt
@@ -1,5 +1,6 @@
 package umc.cozymate.ui.cozy_home.room.room_recommend
 
+import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import umc.cozymate.data.model.response.room.GetRecommendedRoomListResponse
 import umc.cozymate.databinding.VpItemRoomRecommendBinding
@@ -11,8 +12,14 @@ class RoomRecommendVPViewHolder(private val binding: VpItemRoomRecommendBinding)
             with(binding) {
                 tvRoomName.text = item.name
                 tvCriteria1.text = item.equalMemberStatNum.toString()
-                tvMatchRate.text = item.equality.toString()
-                tvHashtag1.text = item.hashtags[0]
+                tvMatchRate.text = "${item.equality.toString()}%"
+                if (item.hashtags.size == 0) {
+                    tvHashtag1.visibility = View.GONE
+                    tvHashtag2.visibility = View.GONE
+                } else if (item.hashtags.size == 1){
+                    tvHashtag1.text = item.hashtags[0]
+                    tvHashtag2.visibility = View.GONE
+                }
             }
         }
 }
