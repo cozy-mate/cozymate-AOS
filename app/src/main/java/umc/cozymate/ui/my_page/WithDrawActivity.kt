178f15886763f224d572ef3ece385664cc5f39ef
iwannacat <ju0808lie@naver.com>
2024-12-07 19:01:16 +0900

    [COZY-430] feat: 글자 짤림 현상 해결

--- app/src/main/java/umc/cozymate/ui/my_page/WithDrawActivity.kt
+++ app/src/main/java/umc/cozymate/ui/my_page/WithDrawActivity.kt
@@ -31,7 +31,7 @@ class WithDrawActivity : AppCompatActivity() {
         binding = ActivityWithdrawBinding.inflate(layoutInflater)
         spf = getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
         setContentView(binding.root)
-        setTextContent()
+        //setTextContent()
         setColor()
         getPreference()
         setClickListener()
@@ -77,15 +77,16 @@ class WithDrawActivity : AppCompatActivity() {
     // 화면 크기에 따라 텍스트 변경
     private fun setTextContent() {
         val originalText = "탈퇴하시면 모든 정보가 사라지며, 모든 데이터는 복구가 불가능해요"
-        val splitText = "탈퇴하시면 모든 정보가 사라지며,\n모든 데이터는 복구가 불가능해요"
+        val splitText = "탈퇴하시면 모든 정보가 사라지며,모든 데이터는 \n복구가 불가능해요"
 
         Log.d(TAG, "로그는 왜 안찍히니")
         // 화면의 가로 크기 확인
         val screenWidth = resources.displayMetrics.widthPixels
         val textWidth =  binding.tvSplitText.paint.measureText(originalText)
-        Log.d(TAG, "로그 screen : ${screenWidth} / text ${textWidth} / result ${textWidth + 65}")
+        val padding = this.resources.displayMetrics.density*65
+        Log.d(TAG, "로그 screen : ${screenWidth} / text ${textWidth} / result ${textWidth + padding}")
 
         // 화면 크기와 텍스트 길이를 비교하여 텍스트 설정
-        binding.tvSplitText.text = if (textWidth+65 > screenWidth) splitText else originalText
+        binding.tvSplitText.text = if (textWidth+padding > screenWidth) splitText else originalText
     }
 }
