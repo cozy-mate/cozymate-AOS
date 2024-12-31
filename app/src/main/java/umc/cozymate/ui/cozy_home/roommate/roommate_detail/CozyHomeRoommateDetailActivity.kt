5f32eabb27440941d55ccb58473451ccf76c477d
youngseojang <youngseojang@gmail.com>
2024-12-31 20:50:21 +0900

    feat: 사용자 검색화면 이동

--- app/src/main/java/umc/cozymate/ui/cozy_home/roommate/roommate_detail/CozyHomeRoommateDetailActivity.kt
+++ app/src/main/java/umc/cozymate/ui/cozy_home/roommate/roommate_detail/CozyHomeRoommateDetailActivity.kt
@@ -1,21 +1,31 @@
 package umc.cozymate.ui.cozy_home.roommate.roommate_detail
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import umc.cozymate.R
+import umc.cozymate.databinding.ActivityCozyHomeRoommateDetailBinding
+import umc.cozymate.ui.cozy_home.roommate.search_roommate.SearchRoommateActivity
 
 class CozyHomeRoommateDetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCozyHomeRoommateDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_cozy_home_roommate_detail)
+        binding = ActivityCozyHomeRoommateDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        // 사용자 검색으로 이동
+        binding.lyRoomMateSearch.setOnClickListener {
+            val intent = Intent(this, SearchRoommateActivity::class.java)
+            startActivity(intent)
         }
     }
 }
