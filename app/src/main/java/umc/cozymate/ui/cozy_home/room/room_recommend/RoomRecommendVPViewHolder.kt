bc0bc5fea5d0228b5aa8cb07770d2f03b6c34971
youngseojang <youngseojang@gmail.com>
2025-01-02 00:26:49 +0900

    feat: 방 추천 멤버스탯 없으면 "???" 뜨기

--- app/src/main/java/umc/cozymate/ui/cozy_home/room/room_recommend/RoomRecommendVPViewHolder.kt
+++ app/src/main/java/umc/cozymate/ui/cozy_home/room/room_recommend/RoomRecommendVPViewHolder.kt
@@ -8,7 +8,7 @@ import umc.cozymate.databinding.VpItemRoomRecommendBinding
 
 class RoomRecommendVPViewHolder(
     private val binding: VpItemRoomRecommendBinding,
-    private val myPrefList: List<String>
+    private val isLifestyleExist: Boolean
 ) :
     RecyclerView.ViewHolder(binding.root) {
 
@@ -51,6 +51,10 @@ class RoomRecommendVPViewHolder(
                     0 -> {
                         tvCriteriaContent1.text = "0명 일치"
                         ivCriteriaIcon1.setImageResource(pref1.redDrawable)
+                        if (isLifestyleExist == false) {
+                            tvCriteriaContent1.text = "???"
+                            ivCriteriaIcon1.setImageResource(pref1.grayDrawable)
+                        }
                     }
 
                     item.numOfArrival -> {
@@ -66,14 +70,17 @@ class RoomRecommendVPViewHolder(
             }
 
             // 선호항목 2
-            val pref2 =
-                Preference.entries.find { it.pref == item.preferenceMatchCountList[1].preferenceName }
+            val pref2 = Preference.entries.find { it.pref == item.preferenceMatchCountList[1].preferenceName }
             if (pref2 != null) {
                 tvCriteria2.text = pref2.displayName
                 when (item.preferenceMatchCountList[1].count) {
                     0 -> {
                         tvCriteriaContent2.text = "0명 일치"
                         ivCrieteriaIcon2.setImageResource(pref2.redDrawable)
+                        if (isLifestyleExist == false) {
+                            tvCriteriaContent2.text = "???"
+                            ivCrieteriaIcon2.setImageResource(pref2.grayDrawable)
+                        }
                     }
 
                     item.numOfArrival -> {
@@ -89,14 +96,17 @@ class RoomRecommendVPViewHolder(
             }
 
             // 선호항목 3
-            val pref3 =
-                Preference.entries.find { it.pref == item.preferenceMatchCountList[2].preferenceName }
+            val pref3 = Preference.entries.find { it.pref == item.preferenceMatchCountList[2].preferenceName }
             if (pref3 != null) {
                 tvCriteria3.text = pref3.displayName
                 when (item.preferenceMatchCountList[2].count) {
                     0 -> {
                         tvCriteriaContent3.text = "0명 일치"
                         ivCrieteriaIcon3.setImageResource(pref3.redDrawable)
+                        if (isLifestyleExist == false) {
+                            tvCriteriaContent3.text = "???"
+                            ivCrieteriaIcon3.setImageResource(pref3.grayDrawable)
+                        }
                     }
 
                     item.numOfArrival -> {
@@ -113,14 +123,17 @@ class RoomRecommendVPViewHolder(
             }
 
             // 선호항목 4
-            val pref4 =
-                Preference.entries.find { it.pref == item.preferenceMatchCountList[3].preferenceName }
+            val pref4 = Preference.entries.find { it.pref == item.preferenceMatchCountList[3].preferenceName }
             if (pref4 != null) {
                 tvCriteria4.text = pref4.displayName
                 when (item.preferenceMatchCountList[3].count) {
                     0 -> {
                         tvCriteriaContent4.text = "0명 일치"
                         ivCrieteriaIcon4.setImageResource(pref4.redDrawable)
+                        if (isLifestyleExist == false) {
+                            tvCriteriaContent4.text = "???"
+                            ivCrieteriaIcon4.setImageResource(pref4.grayDrawable)
+                        }
                     }
 
                     item.numOfArrival -> {
