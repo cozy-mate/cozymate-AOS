b236de59ab502cf3748a69d9186bd73d29eed775
iwannacat <ju0808lie@naver.com>
2025-01-05 22:39:52 +0900

    [COZY-459] feat: 쪽지 밖 선택시 키보드 내리기
    + 마이페이지 스크롤 추가

--- app/src/main/java/umc/cozymate/ui/message/WriteMessageActivity.kt
+++ app/src/main/java/umc/cozymate/ui/message/WriteMessageActivity.kt
@@ -1,12 +1,20 @@
 package umc.cozymate.ui.message
 
+import android.content.Context
 import android.content.Intent
 import android.graphics.Color
+import android.graphics.Rect
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GestureDetectorCompat
 import dagger.hilt.android.AndroidEntryPoint
 import umc.cozymate.data.model.request.ChatRequest
 import umc.cozymate.databinding.ActivityWriteMessageBinding
@@ -20,6 +28,8 @@ class WriteMessageActivity : AppCompatActivity() {
     private val viewModel: ChatViewModel by viewModels()
     private var recipientId : Int = 0
     private var nickname : String = ""
+    private var prev : View? = null
+    private lateinit var mDetector: GestureDetectorCompat
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWriteMessageBinding.inflate(layoutInflater)
@@ -28,6 +38,7 @@ class WriteMessageActivity : AppCompatActivity() {
         checkInput()
         setTextinput()
         setOnClickListener()
+        mDetector = GestureDetectorCompat(this, SingleTapListener())
         recipientId = intent.getIntExtra("recipientId",0)
         nickname = intent.getStringExtra("nickname").toString()
         viewModel.postChatResponse.observe(this){response ->
@@ -40,8 +51,44 @@ class WriteMessageActivity : AppCompatActivity() {
             }
         }
 
+
+
+
     }
 
+    // 밖 터치시 키보드 숨기기
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if(ev!!.action ==  MotionEvent.ACTION_UP)
+            prev = currentFocus
+        val result = super.dispatchTouchEvent(ev)
+        mDetector.onTouchEvent(ev)
+        return result
+    }
+    private inner class SingleTapListener : GestureDetector.SimpleOnGestureListener() {
+        override fun onSingleTapUp(e: MotionEvent): Boolean {
+            // ACTION_UP 이벤트에서 포커스를 가진 뷰가 EditText일 때 터치 영역을 확인하여 키보드를 토글
+            if (e.action == MotionEvent.ACTION_UP && prev is EditText) {
+                val prevFocus = prev ?: return false
+                // 포커를 가진 EditText의 터치 영역 계산
+                val hitRect = Rect()
+                prevFocus.getGlobalVisibleRect(hitRect)
+
+                // 터치 이벤트가 EditText의 터치 영역에 속하지 않을 때 키보드를 숨길지 결정
+                if (!hitRect.contains(e.x.toInt(), e.y.toInt())) {
+                    if (currentFocus is EditText && currentFocus != prevFocus) {
+                        // 터치한 영역의 뷰가 다른 EditText일 때는 키보드를 가리지 않는다.
+                        return false
+                    } else {
+                        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                        // 터치한 영역이 EditText의 터치 영역 밖이면서 다른 EditText가 아닐 때 키보드 hide
+                        imm.hideSoftInputFromWindow(prevFocus.windowToken, 0)
+                        prevFocus.clearFocus()
+                    }
+                }
+            }
+            return super.onSingleTapUp(e)
+        }
+    }
     private fun checkInput() {
         val inputFlag = !binding.etInputMessage.text.isNullOrEmpty()
         binding.btnInputButton.isEnabled = inputFlag
