0540dff623bfa9de837b08cb43f8450528b4d439
youngseojang <youngseojang@gmail.com>
2025-01-13 23:20:47 +0900

    feat: [찜한 방/룸메] 리스트 수정

--- app/src/main/java/umc/cozymate/ui/my_page/MyFavoriteActivity.kt
+++ app/src/main/java/umc/cozymate/ui/my_page/MyFavoriteActivity.kt
@@ -14,6 +14,7 @@ import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import umc.cozymate.databinding.ActivityMyFavoriteBinding
 import umc.cozymate.ui.MainActivity
+import umc.cozymate.ui.cozy_home.room.room_detail.VerticalSpaceItemDecoration
 import umc.cozymate.ui.cozy_home.room_detail.RoomDetailActivity
 import umc.cozymate.ui.cozy_home.roommate.roommate_detail.CozyHomeRoommateDetailActivity
 import umc.cozymate.ui.pop_up.OneButtonPopup
@@ -43,6 +44,7 @@ class MyFavoriteActivity : AppCompatActivity() {
     }
 
     fun setupRVAdapter() {
+        // 찜한 룸메이트
         membersAdapter = FavoriteRoommateRVAdapter(emptyList()) { memberId ->
             val intent = Intent(this, CozyHomeRoommateDetailActivity::class.java).apply {
                 putExtra("member_id", memberId) // 멤버 아이디 전달
@@ -55,6 +57,10 @@ class MyFavoriteActivity : AppCompatActivity() {
         }
         binding.rvFavoriteRoommate.adapter = membersAdapter
         binding.rvFavoriteRoommate.layoutManager = LinearLayoutManager(this)
+        binding.rvFavoriteRoommate.addItemDecoration(
+            VerticalSpaceItemDecoration(32)
+        )
+        // 찜한 방
         roomsAdapter = FavoriteRoomRVAdapter(emptyList()) { roomId ->
             val intent = Intent(this, RoomDetailActivity::class.java).apply {
                 putExtra(RoomDetailActivity.ARG_ROOM_ID, roomId) // 방 아이디 전달
@@ -67,6 +73,9 @@ class MyFavoriteActivity : AppCompatActivity() {
         }
         binding.rvFavoriteRoom.adapter = roomsAdapter
         binding.rvFavoriteRoom.layoutManager = LinearLayoutManager(this)
+        binding.rvFavoriteRoom.addItemDecoration(
+            VerticalSpaceItemDecoration(32)
+        )
     }
 
     fun setupObservers() {
