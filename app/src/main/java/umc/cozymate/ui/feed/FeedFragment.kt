b3e1dda3ebe78520faa44c30411c0c19bf296248
iwannacat <ju0808lie@naver.com>
2025-02-18 21:01:29 +0900

    백업용

--- app/src/main/java/umc/cozymate/ui/feed/FeedFragment.kt
+++ app/src/main/java/umc/cozymate/ui/feed/FeedFragment.kt
@@ -5,18 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
 import umc.cozymate.databinding.FragmentFeedBinding
 
+@AndroidEntryPoint
 class FeedFragment : Fragment() {
-    private var _binding: FragmentFeedBinding? = null
-    private val binding get() = _binding!!
+    private val TAG  = this.javaClass.simpleName
+    lateinit var binding : FragmentFeedBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentFeedBinding.inflate(inflater, container, false)
+        binding = FragmentFeedBinding.inflate(inflater, container, false)
 
         return binding.root
     }
