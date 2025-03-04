a133f82b6b76bdaec9778da1cd26a99bb07077b1
youngseojang <youngseojang@gmail.com>
2025-03-04 16:14:54 +0900

    feat: 자동 업데이트 확인 기능 구현

--- app/src/main/java/umc/cozymate/ui/cozy_home/home/CozyHomeMainFragment.kt
+++ app/src/main/java/umc/cozymate/ui/cozy_home/home/CozyHomeMainFragment.kt
@@ -1,18 +1,26 @@
 package umc.cozymate.ui.cozy_home.home
 
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 import umc.cozymate.R
@@ -35,6 +43,8 @@ import umc.cozymate.util.StatusBarUtil
 
 @AndroidEntryPoint
 class CozyHomeMainFragment : Fragment() {
+    private val UPDATE_REQUEST_CODE = 1001
+    private val updateManager by lazy { AppUpdateManagerFactory.create(requireActivity()) }
     private var _binding: FragmentCozyHomeMainBinding? = null
     private val binding get() = _binding!!
     private val viewModel: CozyHomeViewModel by viewModels()
@@ -78,12 +88,15 @@ class CozyHomeMainFragment : Fragment() {
             initView()
             // 새로고침 설정
             onRefresh()
+            // 업데이트 체크
+            checkForUpdate()
             binding.refreshLayout.isRefreshing = false
         }
     }
 
     override fun onResume() {
         super.onResume()
+        checkForUpdate()
         binding.refreshLayout.isRefreshing = true
         initState()
         initView()
@@ -93,6 +106,39 @@ class CozyHomeMainFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    // 새로운 앱 버전이 있는지 확인합니다.
+    private fun checkForUpdate() {
+        val appUpdateInfoTask = updateManager.appUpdateInfo
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE &&
+                appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)) {
+                AlertDialog.Builder(requireContext())
+                    .setTitle("")
+                    .setMessage("새로운 버전이 출시되었습니다. 업데이트하시겠습니까?")
+                    .setPositiveButton("확인") { _, _ ->
+                        try {
+                            // 강제 업데이트 요청
+                            //updateManager.startUpdateFlowForResult(appUpdateInfo, AppUpdateType.IMMEDIATE, requireActivity(), UPDATE_REQUEST_CODE )
+                            // 구글 플레이스토어로 리다이렉트
+                            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=umc.cozymate")))
+                        } catch(e: ActivityNotFoundException) {
+                            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/apps/test/umc.cozymate")))
+                        }
+                    }
+                    .setNegativeButton("나중에") { dialog, _ -> dialog.dismiss() }
+                    .show()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == UPDATE_REQUEST_CODE && resultCode != Activity.RESULT_OK) {
+            // 업데이트 실패 처리
+            Toast.makeText(requireActivity(), "업데이트 실패", Toast.LENGTH_SHORT).show()
+        }
     }
 
     private fun onRefresh() {
@@ -191,7 +237,10 @@ class CozyHomeMainFragment : Fragment() {
                         replace(R.id.received_invitation_container, ReceivedInvitationComponent())
                         replace(R.id.recommended_room_container, RecommendedRoomComponent())
                         replace(R.id.recommended_roommate_container, RecommendedRoommateComponent())
-                        replace(R.id.received_join_request_container, ReceivedJoinRequestComponent())
+                        replace(
+                            R.id.received_join_request_container,
+                            ReceivedJoinRequestComponent()
+                        )
                         commit()
                     }
                     // 안 보이는 컴포넌트
@@ -210,7 +259,10 @@ class CozyHomeMainFragment : Fragment() {
                         replace(R.id.received_invitation_container, ReceivedInvitationComponent())
                         replace(R.id.recommended_room_container, RecommendedRoomComponent())
                         replace(R.id.recommended_roommate_container, RecommendedRoommateComponent())
-                        replace(R.id.received_join_request_container, ReceivedJoinRequestComponent())
+                        replace(
+                            R.id.received_join_request_container,
+                            ReceivedJoinRequestComponent()
+                        )
                         commit()
                     }
                     // 안 보이는 컴포넌트
