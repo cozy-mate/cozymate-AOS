192283ff2dcac2716ab0028f7bdd30bdc091b19a
youngseojang <youngseojang@gmail.com>
2025-04-01 18:20:46 +0900

    feat: 약관동의 바텀시트 버튼 동작 구현

--- app/src/main/java/umc/cozymate/ui/onboarding/OnboardingSelectingPreferenceFragment.kt
+++ app/src/main/java/umc/cozymate/ui/onboarding/OnboardingSelectingPreferenceFragment.kt
@@ -1,14 +1,18 @@
 package umc.cozymate.ui.onboarding
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import dagger.hilt.android.AndroidEntryPoint
 import umc.cozymate.R
 import umc.cozymate.data.model.entity.PreferenceList
@@ -18,26 +22,36 @@ import umc.cozymate.util.PreferenceNameToId
 
 @AndroidEntryPoint
 class OnboardingSelectingPreferenceFragment : Fragment() {
-
     private val TAG = this.javaClass.simpleName
     private lateinit var binding: FragmentOnboardingSelectingPreferenceBinding
     private val viewModel: OnboardingViewModel by viewModels()
+    private lateinit var chips: List<TextView>
+    private val selectedChips = mutableListOf<TextView>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_onboarding_selecting_preference, container, false)
-        binding.viewModel = viewModel
-        binding.lifecycleOwner = viewLifecycleOwner
-
-        setupTextViews()
-
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_onboarding_selecting_preference,
+            container,
+            false
+        )
         return binding.root
     }
 
-    private fun setupTextViews() {
-        val textViews = listOf(
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        binding.btnNext.isEnabled = false
+        setupChips()
+        setupNextBtn()
+    }
+
+    private fun setupChips() {
+        chips = listOf(
             binding.chipBirthYear,
             binding.chipAdmissionYear,
             binding.chipMajor,
@@ -63,45 +77,102 @@ class OnboardingSelectingPreferenceFragment : Fragment() {
             binding.chipDrinkingFrequency,
             binding.chipPersonality
         )
-
-        val selectedChips = mutableListOf<TextView>()
-
-        binding.btnNext.isEnabled = false
-
-        textViews.forEach { textView ->
-            textView.setOnClickListener {
-                textView.isSelected = !textView.isSelected
-
-                if (textView.isSelected) {
-                    selectedChips.add(textView)
-                } else {
-                    selectedChips.remove(textView)
-                }
-
+        chips.forEach { c ->
+            c.setOnClickListener {
+                c.isSelected = !c.isSelected
+                if (c.isSelected) {
+                    selectedChips.add(c)
+                } else selectedChips.remove(c)
+                // 칩 4개 넘게 선택하면 비활성화
                 if (selectedChips.size > 4) {
                     Toast.makeText(context, "선호항목을 4개 선택해주세요", Toast.LENGTH_SHORT).show()
+                    c.isSelected = !c.isSelected
                     binding.btnNext.isEnabled = false
                 }
-
-                viewModel.updateSelectedElementCount(textView.isSelected)
+                viewModel.updateSelectedElementCount(c.isSelected)
             }
         }
+    }
 
+    fun setupNextBtn() {
         binding.btnNext.setOnClickListener {
             if (selectedChips.size == 4) {
                 val preferences = PreferenceList(selectedChips.map { PreferenceNameToId(it.text.toString()) } as ArrayList<String>)
                 viewModel.setPreferences(preferences)
-
-                viewModel.postPreference()
-
-                parentFragmentManager.beginTransaction()
-                    .replace(R.id.fragment_onboarding, OnboardingSummaryFragment()) // 화면 이동
-                    .addToBackStack(null)
-                    .commit()
-            }
-            else {
+                setupBottomSheet()
+            } else {
                 Toast.makeText(context, "선호항목을 4개 선택해주세요", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+
+    fun setupBottomSheet() {
+        var checked1: Boolean = false
+        var checked2: Boolean = false
+        var checkedAll: Boolean = false
+        val sheetBehavior = BottomSheetBehavior.from(binding.includeBs.bottomSheetAgreement)
+        sheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        with(binding.includeBs) {
+            btnSeeAgreement1.setOnClickListener() {
+                val url = "https://google.com"
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(intent)
+            }
+            btnSeeAgreement2.setOnClickListener() {
+                val url = "https://google.com"
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(intent)
+            }
+            btnCheck1.setOnClickListener() {
+                checked1 = btnCheck1.isSelected
+                checked2 = btnCheck2.isSelected
+                btnCheck1.isSelected = !checked1
+                updateCheckAllState(checked1, checked2)
+            }
+            btnCheck2.setOnClickListener() {
+                checked1 = btnCheck1.isSelected
+                checked2 = btnCheck2.isSelected
+                btnCheck2.isSelected = !checked2
+                updateCheckAllState(checked1, checked2)
+            }
+            btnCheckAll.setOnClickListener() {
+                checkedAll = btnCheckAll.isSelected
+                btnCheckAll.isSelected = !checkedAll
+                if (checkedAll) {
+                    checked1 = false
+                    checked2 = false
+                    btnCheck1.isSelected = checked1
+                    btnCheck2.isSelected = checked2
+                    btnNext.isEnabled = false
+                    checkedAll = false
+                } else {
+                    checked1 = true
+                    checked2 = true
+                    btnCheck1.isSelected = checked1
+                    btnCheck2.isSelected = checked2
+                    btnNext.isEnabled = true
+                    checkedAll = true
+                }
+            }
+            btnNext.setOnClickListener() {
+                if (btnCheckAll.isSelected) {
+                    goToSummaryFragment()
+                }
+            }
+        }
+    }
+
+    fun updateCheckAllState(chk1: Boolean, chk2: Boolean) {
+        val isCheckedAll = chk1 && chk2
+        binding.includeBs.btnCheckAll.isSelected = isCheckedAll
+        binding.includeBs.btnNext.isEnabled = isCheckedAll
+    }
+
+    fun goToSummaryFragment() {
+        viewModel.postPreference()
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.fragment_onboarding, OnboardingSummaryFragment()) // 화면 이동
+            .addToBackStack(null)
+            .commit()
     }
 }
