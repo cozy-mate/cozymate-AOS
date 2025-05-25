caf1fa1ca5613e98f969981a41422bb78552c840
youngseojang <youngseojang@gmail.com>
2025-05-25 22:38:54 +0900

    feat: GA 상수 코드 작성

---
+++ app/src/main/java/umc/cozymate/util/AnalyticsConstants.kt
@@ -0,0 +1,121 @@
+package umc.cozymate.util
+
+class AnalyticsConstants {
+
+    object Keys {
+        const val CATEGORY = "category"
+        const val ACTION = "action"
+        const val LABEL = "label"
+    }
+
+    object Category {
+        // 온보딩
+        const val ONBOARDING1 = "onboarding1"
+        const val ONBOARDING2 = "onboarding2"
+        const val ONBOARDING3 = "onboarding3"
+        const val ONBOARDING4 = "onboarding4"
+        const val ONBOARDING5 = "onboarding5"
+
+        // 코지홈
+        const val HOME_HEADER = "home_header"
+        const val HOME_CONTENT = "home_content"
+
+        // 라이프스타일
+        const val LIFE_STYLE = "life_style"
+
+        // 룸메이트 추천
+        const val CONTENT_MATE = "content_mate"
+        const val MATE_DETAIL = "mate_detail"
+
+        // 방 추천
+        const val CONTENT_ROOM = "content_room"
+        const val ROOM_DETAIL = "room_detail"
+    }
+
+    object Action {
+        const val INPUT_BOX = "input_box"
+        const val BUTTON_CLICK = "button_click"
+        const val SESSION_TIME = "session_time"
+        const val GESTURE = "gesture"
+    }
+
+    object Label {
+        const val AGREE_1 = "agree_1"
+        const val AGREE_2 = "agree_2"
+        const val AGREE_ALL = "agree_all"
+        const val AIRCON = "aircon"
+        const val BIRTH = "birth"
+        const val CLEAN_FREQ = "clean_freq"
+        const val CLEAN_SENSITIVITY = "clean_sensitivity"
+        const val CLOSENESS = "closeness"
+        const val DORM_PASS = "dorm_pass"
+        const val DRINKING_FREQ = "drinking_freq"
+        const val EAT = "eat"
+        const val EMAIL = "email"
+        const val EMAIL_CODE = "email_code"
+        const val GAME = "game"
+        const val GENDER = "gender"
+        const val HEATER = "heater"
+        const val ITEM_SHARING = "item_sharing"
+        const val LIFESTYLE = "lifestyle"
+        const val LIGHT_OFF = "light_off"
+        const val MAJOR = "major"
+        const val MBTI = "mbti"
+        const val MORNING = "morning"
+        const val NAME = "name"
+        const val NIGHT = "night"
+        const val NOISE = "noise"
+        const val OKAY = "okay"
+        const val PERSONALITY = "personality"
+        const val PHONE_CALL = "phone_call"
+        const val SLEEP_HABIT = "sleep_habit"
+        const val SMOKE = "smoke"
+        const val STUDENT_ID = "student_id"
+        const val STUDY = "study"
+        const val UNIV = "univ"
+    }
+
+    object Event {
+        const val INPUT_BOX_UNIV = "input_box_univ"
+        const val INPUT_BOX_MAJOR = "input_box_major"
+        const val INPUT_BOX_EMAIL = "input_box_email"
+        const val INPUT_BOX_EMAIL_CODE = "input_box_email_code"
+        const val BUTTON_CLICK_EMAIL = "button_click_email"
+        const val BUTTON_CLICK_EMAIL_CODE = "button_click_email_code"
+        const val SESSION_TIME = "session_time"
+
+        const val INPUT_BOX_NAME = "input_box_name"
+        const val INPUT_BOX_GENDER = "input_box_gender"
+        const val INPUT_BOX_BIRTH = "input_box_birth"
+        const val BUTTON_CLICK_NAME = "button_click_name"
+
+        const val BUTTON_CLICK_BIRTH = "button_click_birth"
+        const val BUTTON_CLICK_STUDENT_ID = "button_click_student_id"
+        const val BUTTON_CLICK_MAJOR = "button_click_major"
+        const val BUTTON_CLICK_DORM_PASS = "button_click_dorm_pass"
+        const val BUTTON_CLICK_MORNING = "button_click_morning"
+        const val BUTTON_CLICK_NIGHT = "button_click_night"
+        const val BUTTON_CLICK_LIGHT_OFF = "button_click_light_off"
+        const val BUTTON_CLICK_SMOKE = "button_click_smoke"
+        const val BUTTON_CLICK_SLEEP_HABIT = "button_click_sleep_habit"
+        const val BUTTON_CLICK_AIRCON = "button_click_aircon"
+        const val BUTTON_CLICK_HEATER = "button_click_heater"
+        const val BUTTON_CLICK_LIFESTYLE = "button_click_lifestyle"
+        const val BUTTON_CLICK_CLOSENESS = "button_click_closeness"
+        const val BUTTON_CLICK_ITEM_SHARING = "button_click_item_sharing"
+        const val BUTTON_CLICK_GAME = "button_click_game"
+        const val BUTTON_CLICK_PHONE_CALL = "button_click_phone_call"
+        const val BUTTON_CLICK_STUDY = "button_click_study"
+        const val BUTTON_CLICK_EAT = "button_click_eat"
+        const val BUTTON_CLICK_CLEAN_SENSITIVITY = "button_click_clean_sensitivity"
+        const val BUTTON_CLICK_NOISE = "button_click_noise"
+        const val BUTTON_CLICK_CLEAN_FREQ = "button_click_clean_freq"
+        const val BUTTON_CLICK_DRINKING_FREQ = "button_click_drinking_freq"
+        const val BUTTON_CLICK_PERSONALITY = "button_click_personality"
+        const val BUTTON_CLICK_MBTI = "button_click_mbti"
+        const val BUTTON_CLICK_OKAY = "button_click_okay"
+        const val BUTTON_CLICK_AGREE_ALL = "button_click_agree_all"
+        const val BUTTON_CLICK_AGREE_1 = "button_click_agree_1"
+        const val BUTTON_CLICK_AGREE_2 = "button_click_agree_2"
+    }
+}
