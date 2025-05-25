e3fcc84e202964e5505a9586d9dbc3e359e8ee92
youngseojang <youngseojang@gmail.com>
2025-05-25 22:39:38 +0900

    feat: GA 이벤트 로그 추가해주는 헬퍼 구현

---
+++ app/src/main/java/umc/cozymate/util/AnalyticsEventLogger.kt
@@ -0,0 +1,26 @@
+package umc.cozymate.util
+
+import android.os.Bundle
+import com.google.firebase.Firebase
+import com.google.firebase.analytics.analytics
+
+object AnalyticsEventLogger {
+    private val analytics by lazy {
+        Firebase.analytics
+    }
+
+    fun logEvent(
+        eventName: String,
+        category: String? = null,
+        action: String? = null,
+        label: String? = null
+    ) {
+        val params = Bundle().apply {
+            category?.let { putString(AnalyticsConstants.Keys.CATEGORY, it) }
+            action?.let { putString(AnalyticsConstants.Keys.ACTION, it) }
+            label?.let { putString(AnalyticsConstants.Keys.LABEL, it) }
+        }
+
+        analytics.logEvent(eventName, params)
+    }
+}
