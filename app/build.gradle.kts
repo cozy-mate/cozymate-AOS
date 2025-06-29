f55e5d8bf075c557c28685b50efd67080019479f
youngseojang <youngseojang@gmail.com>
2025-06-29 19:01:42 +0900

    fix: 코지봇 바텀네비 삭제

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "umc.cozymate"
         minSdk = 26
         targetSdk = 34
-        versionCode = 19
-        versionName = "1.19"
+        versionCode = 20
+        versionName = "1.20"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
