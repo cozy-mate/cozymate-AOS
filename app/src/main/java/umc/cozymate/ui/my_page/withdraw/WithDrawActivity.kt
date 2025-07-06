7bc50d3b453f327c014e756439af67d324ff1e73
iwannacat <ju0808lie@naver.com>
2025-07-06 22:16:06 +0900

    [COZY-644] feat: 탈퇴화면 외부 클릭시 키보드 내려가도록 설정

--- app/src/main/java/umc/cozymate/ui/my_page/withdraw/WithDrawActivity.kt
+++ app/src/main/java/umc/cozymate/ui/my_page/withdraw/WithDrawActivity.kt
@@ -5,11 +5,17 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.content.res.ColorStateList
 import android.graphics.Color
+import android.graphics.Rect
 import android.os.Bundle
 import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GestureDetectorCompat
 import androidx.core.widget.CompoundButtonCompat
 import androidx.lifecycle.Observer
 import dagger.hilt.android.AndroidEntryPoint
@@ -24,14 +30,19 @@ class WithDrawActivity : AppCompatActivity() {
     private val TAG = this.javaClass.simpleName
     private lateinit var binding: ActivityWithdrawBinding
     private lateinit var spf : SharedPreferences
+    private lateinit var mDetector: GestureDetectorCompat
     private var reason : String = ""
     private var memberId : Int = 0
     private var nickname: String = ""
     private val viewModel : SplashViewModel by viewModels()
+    private var prev : View? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityWithdrawBinding.inflate(layoutInflater)
         spf = getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+        mDetector = GestureDetectorCompat(this, SingleTapListener())
+
         setContentView(binding.root)
         StatusBarUtil.updateStatusBarColor(this, Color.WHITE) // 상단바 색상 수정
         setTextContent()
@@ -101,5 +112,40 @@ class WithDrawActivity : AppCompatActivity() {
 
         // 화면 크기와 텍스트 길이를 비교하여 텍스트 설정
         binding.tvSplitText.text = if (textWidth+padding > screenWidth) splitText else originalText
+    }
+
+    // 밖 터치시 키보드 숨기기
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if(ev!!.action ==  MotionEvent.ACTION_UP)
+            prev = currentFocus
+        val result = super.dispatchTouchEvent(ev)
+        mDetector.onTouchEvent(ev)
+        return result
+    }
+
+    private inner class SingleTapListener : GestureDetector.SimpleOnGestureListener() {
+        override fun onSingleTapUp(e: MotionEvent): Boolean {
+            // ACTION_UP 이벤트에서 포커스를 가진 뷰가 EditText일 때 터치 영역을 확인하여 키보드를 토글
+            if (e.action == MotionEvent.ACTION_UP && prev is EditText) {
+                val prevFocus = prev ?: return false
+                // 포커를 가진 EditText의 터치 영역 계산
+                val hitRect = Rect()
+                prevFocus.getGlobalVisibleRect(hitRect)
+
+                // 터치 이벤트가 EditText의 터치 영역에 속하지 않을 때 키보드를 숨길지 결정
+                if (!hitRect.contains(e.x.toInt(), e.y.toInt())) {
+                    if (currentFocus is EditText && currentFocus != prevFocus) {
+                        // 터치한 영역의 뷰가 다른 EditText일 때는 키보드를 가리지 않는다.
+                        return false
+                    } else {
+                        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                        // 터치한 영역이 EditText의 터치 영역 밖이면서 다른 EditText가 아닐 때 키보드 hide
+                        imm.hideSoftInputFromWindow(prevFocus.windowToken, 0)
+                        prevFocus.clearFocus()
+                    }
+                }
+            }
+            return super.onSingleTapUp(e)
+        }
     }
 }
